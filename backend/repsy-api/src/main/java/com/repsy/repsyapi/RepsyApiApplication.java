ca86c5bcb4680522cb33c331f765632c83a5f76e
baran61 <azak.baran99@gmail.com>
2025-04-22 03:28:55 +0300

    Initial project setup: Spring Boot + Docker Compose with PostgreSQL & MinIO using Colima

---
+++ backend/repsy-api/src/main/java/com/repsy/repsyapi/RepsyApiApplication.java
@@ -0,0 +1,13 @@
+package com.repsy.repsyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepsyApiApplication.class, args);
+	}
+
+}
